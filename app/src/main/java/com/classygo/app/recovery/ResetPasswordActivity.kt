b225e12fdc1ed75d2183765fa6ea463de22549b4
clevmania <ernestcleverest@yahoo.com>
2020-10-17 19:40:39 +0100

    feat: implement the view model

--- app/src/main/java/com/classygo/app/recovery/ResetPasswordActivity.kt
+++ app/src/main/java/com/classygo/app/recovery/ResetPasswordActivity.kt
@@ -1,52 +1,50 @@
 package com.classygo.app.recovery
 
 import android.app.AlertDialog
-import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.classygo.app.R
-import com.classygo.app.startup.SplashActivity
+import com.classygo.app.utils.EventObserver
 import com.classygo.app.utils.validateEmail
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import kotlinx.android.synthetic.main.activity_reset_password.*
 
 class ResetPasswordActivity : AppCompatActivity() {
-    private val auth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
+
+    private val viewModel: ResetPasswordViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reset_password)
 
         mbRequestReset.setOnClickListener { validateEmail() }
+
+        viewModel.requestReset.observe(this, EventObserver {
+            when (it) {
+                is ResetSuccess -> {
+                    showSuccessDialog()
+                }
+                is RequestError -> {
+                    showAlertDialog(it.error)
+                }
+                is InvalidUser -> {
+                    showAlertDialog(getString(R.string.no_such_account))
+                }
+            }
+        })
     }
 
     private fun validateEmail() {
         val mailAddress = tilEmailAddress.validateEmail()
         mailAddress?.let {
-            requestPassWordReset(email = it)
+            viewModel.requestPassWordReset(email = it)
         }
     }
 
-    private fun requestPassWordReset(email: String) {
-        auth.sendPasswordResetEmail(email)
-            .addOnCompleteListener { task ->
-                if(task.isSuccessful) {
-                    showSuccessDialog()
-                }
-            }.addOnFailureListener {
-                if (it is FirebaseAuthInvalidUserException) {
-                    showAlertDialog(getString(R.string.no_such_account))
-                } else {
-                    it.localizedMessage?.let { error -> showAlertDialog(error) }
-                }
-            }
-    }
-
-    private fun showSuccessDialog(){
+    private fun showSuccessDialog() {
         val builder = AlertDialog.Builder(this)
         builder.setCancelable(false)
-        builder.setTitle(getString(R.string.reset_mail_sent) )
+        builder.setTitle(getString(R.string.reset_mail_sent))
         builder.setMessage(getString(R.string.reset_sent))
         builder.setPositiveButton(getString(R.string.back_to_login)) { dialogInterface, _ ->
             dialogInterface.dismiss()
