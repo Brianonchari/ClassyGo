8c337ada989e25de2770c7b1d0a38d6a7b2554cb
bright <brightahedor@gmail.com>
2020-10-17 13:31:09 +0100

    added the trip activity

---
+++ app/src/main/java/com/classygo/app/trip/AllTripsActivity.kt
@@ -0,0 +1,25 @@
+package com.classygo.app.trip
+
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.classygo.app.R
+import kotlinx.android.synthetic.main.home_toolbar.*
+
+class AllTripsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_trips)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = ""
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
