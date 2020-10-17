ab7afbc6e14d48e1f1866af2efefdff6efbb379e
bright <brightahedor@gmail.com>
2020-10-17 18:43:28 +0100

    added picasso

--- app/src/main/java/com/classygo/app/trip/AllTripsActivity.kt
+++ app/src/main/java/com/classygo/app/trip/AllTripsActivity.kt
@@ -4,16 +4,54 @@ import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.classygo.app.R
+import com.classygo.app.model.Trip
+import com.classygo.app.model.TripLocation
+import kotlinx.android.synthetic.main.activity_all_trips.*
 import kotlinx.android.synthetic.main.home_toolbar.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class AllTripsActivity : AppCompatActivity() {
+
+    private val feedItems = ArrayList<Trip>()
+    private var baseAdapter: TripFeedAdapter? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_all_trips)
 
-        setSupportActionBar(toolbar)
+        setSupportActionBar(toolbarHome as Toolbar?)
         supportActionBar?.title = ""
+
+        setUpRecyclerView()
+    }
+
+
+    //MARK: set the recycler view layouts
+    private fun setUpRecyclerView() {
+        baseAdapter = TripFeedAdapter(this, feedItems)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.isNestedScrollingEnabled = true
+        val linearLayoutManager = LinearLayoutManager(this)
+        linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+        recyclerView.layoutManager = linearLayoutManager
+        recyclerView.adapter = baseAdapter
+
+        addDummyData()
+    }
+
+    private fun addDummyData() {
+        val route = TripLocation()
+        val trip = Trip("", route, "", "https://li1.modland.net/euro-truck-simulator-2/cars-bus/ets2_20200318_104918_00_ModLandNet.png", "12 Hours", Date(), Date())
+
+        feedItems.add(trip)
+        feedItems.add(trip)
+        feedItems.add(trip)
+
+        baseAdapter?.notifyDataSetChanged()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
---
+++ app/src/main/java/com/classygo/app/trip/TripFeedAdapter.kt
@@ -0,0 +1,85 @@
+package com.classygo.app.trip
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.classygo.app.R
+import com.classygo.app.model.Trip
+import com.google.android.material.button.MaterialButton
+import com.squareup.picasso.Picasso
+
+
+/**
+ * Created by Monarchy on 17/10/2020.
+ */
+
+class TripFeedAdapter(private val activity: Activity, private var items: List<Trip>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val constant = 100
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        var viewHolder: RecyclerView.ViewHolder? = null
+        val inflater = LayoutInflater.from(viewGroup.context)
+        when (viewType) {
+            constant -> {
+                val viewHolderItem = inflater.inflate(R.layout.trip_card_item, viewGroup, false)
+                viewHolder = TripViewHolder(viewHolderItem)
+            }
+        }
+        return viewHolder!!
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder.itemViewType) {
+            constant -> {
+                val viewHolderMallBanner = holder as TripViewHolder
+                configureTripViewHolder(viewHolderMallBanner, items[position], activity)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return this.items.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when {
+            items[position] is Trip -> constant
+            else -> -1
+        }
+    }
+
+    // MARK: configure the device
+    private fun configureTripViewHolder(
+        viewHolder: TripViewHolder,
+        data: Trip,
+        activity: Activity
+    ) {
+        val parent = viewHolder.itemView
+        val imageViewTrip = viewHolder.imageViewTrip
+
+        if (data.image?.isNotEmpty()!!) {
+            Picasso.get().load(data.image).into(imageViewTrip)
+        }
+
+    }
+}
+
+//MARK: view holder of the trip
+class TripViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var imageViewTrip: ImageView? = null
+    var textViewTitle: TextView? = null
+    var textViewEtaInfo: TextView? = null
+    var buttonJoinTrip: MaterialButton? = null
+
+    init {
+        imageViewTrip = itemView.findViewById(R.id.imageViewTrip)
+        textViewTitle = itemView.findViewById(R.id.textViewTitle)
+        textViewEtaInfo = itemView.findViewById(R.id.textViewEtaInfo)
+        buttonJoinTrip = itemView.findViewById(R.id.buttonJoinTrip)
+    }
+}
