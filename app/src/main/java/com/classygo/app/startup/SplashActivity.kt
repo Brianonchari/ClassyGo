6ff30e8d8040cfca16d5e1f89894106f54ed363b
carluvy <37089288+carluvy@users.noreply.github.com>
2020-10-18 19:41:39 +0300

    Build SplashActivity Layout

--- app/src/main/java/com/classygo/app/startup/SplashActivity.kt
+++ app/src/main/java/com/classygo/app/startup/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
+
 import com.classygo.app.R
 import com.classygo.app.startup.setup.LoginActivity
 
@@ -12,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
     private val SPLASH_TIMEOUT = 3000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
         Handler().postDelayed({
             val i = Intent(this@SplashActivity, LoginActivity::class.java)
             startActivity(i)
