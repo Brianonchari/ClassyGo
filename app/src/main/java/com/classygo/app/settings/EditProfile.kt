74ad143e96015619a1e6d7b8d9a112b9d8af96d6
bright <brightahedor@gmail.com>
2020-10-27 00:34:00 +0100

    added edit profile

--- app/src/main/java/com/classygo/app/settings/EditProfile.kt
+++ app/src/main/java/com/classygo/app/settings/EditProfile.kt
@@ -5,18 +5,20 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import com.classygo.app.R
-import kotlinx.android.synthetic.main.activity_new_trip.*
+import kotlinx.android.synthetic.main.activity_edit_profile.*
+import kotlinx.android.synthetic.main.activity_edit_profile.toolbar
+import kotlinx.android.synthetic.main.toolbar.*
 
-class ProfileActivity : AppCompatActivity() {
+
+class EditProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_edit_profile)
 
         setSupportActionBar(toolbar as Toolbar?)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = ""
-        textViewTitle.text = getString(R.string.title_profile)
-
+        textViewTitle.text = getString(R.string.title_edit_profile)
 
 
 
