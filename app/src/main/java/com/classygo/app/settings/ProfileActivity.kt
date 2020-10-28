7e1751deec77d82a604d39ac23a22690f352793c
carluvy <37089288+carluvy@users.noreply.github.com>
2020-10-28 19:43:50 +0300

    added logic to navigate to onBoard screen on logout

--- app/src/main/java/com/classygo/app/settings/ProfileActivity.kt
+++ app/src/main/java/com/classygo/app/settings/ProfileActivity.kt
@@ -5,21 +5,29 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import com.classygo.app.R
+import com.classygo.app.databinding.ActivityProfileBinding
+import com.classygo.app.onboard.OnBoardActivity
 import com.classygo.app.utils.launchActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_new_trip.*
 import kotlinx.android.synthetic.main.activity_new_trip.toolbar
-import kotlinx.android.synthetic.main.activity_profile.*
+
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityProfileBinding
+    private lateinit var mCurrentUser : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         setSupportActionBar(toolbar as Toolbar?)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = ""
         textViewTitle.text = getString(R.string.title_profile)
         changePassword()
+        logOut()
 
 
 
@@ -34,8 +42,18 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun changePassword() {
-        cardViewSecurity.setOnClickListener {
+        binding.cardViewSecurity.setOnClickListener {
             launchActivity<ChangePasswordActivity>()
+            finish()
+        }
+
+    }
+
+    private fun logOut() {
+        mCurrentUser = FirebaseAuth.getInstance()
+        binding.cardViewLogOut.setOnClickListener {
+            mCurrentUser.signOut()
+            launchActivity<OnBoardActivity>()
             finish()
         }
 
